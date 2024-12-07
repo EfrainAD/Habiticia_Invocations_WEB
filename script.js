b800c94c05cf6e3cf1250e96b457cba5ae171600
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2024-12-06 23:28:17 -0500

    Feature: made page that will show you the best quest to get by what stat gear you want

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const stats = ['str', 'con', 'int', 'per']
+
 // -- Util Functions -- ||
 const createTopGearsObj = (stats, gearTypes) => {
    const gearObj = {}
@@ -9,7 +11,7 @@ const createTopGearsObj = (stats, gearTypes) => {
          gearObj[stat][gearType] = {}
       })
 
-      gearObj.weapon = { oneHanded: {}, twoHanded: {} }
+      gearObj[stat].weapon = { oneHanded: {}, twoHanded: {} }
    })
 
    return gearObj
@@ -18,10 +20,8 @@ const createTopGearsObj = (stats, gearTypes) => {
 const isObjEmpty = (obj) => Object.keys(obj).length < 1
 
 const parseQuest = (habatica) => {
-   const stats = ['str', 'con', 'int', 'per']
-   const gearTypes = habatica.gearTypes
    const gears = habatica.gear.flat
-   const topGear = createTopGearsObj(stats, gearTypes)
+   const topGear = createTopGearsObj(stats, habatica.gearTypes)
    const questNames = Object.keys(habatica.quests)
 
    const allQuests = questNames.map((questName) => habatica.quests[questName])
@@ -39,7 +39,7 @@ const parseQuest = (habatica) => {
       )
 
       gears.forEach((gear) => {
-         gear.quest = { key: quest.key, text: quest.text }
+         gear.quest = { key: quest.key, text: quest.text, type: quest.category }
       })
 
       return acc.concat(gears)
@@ -52,20 +52,108 @@ const parseQuest = (habatica) => {
 
       stats.forEach((stat) => {
          if (gear[stat] > 0) {
-            if (isObjEmpty(topGear[stat][gearType])) {
-               topGear[stat][gearType] = gear
-            } else if (gear[stat] > topGear[stat][gearType][stat]) {
-               topGear[stat][gearType] = gear
+            if (gear.type === 'weapon') {
+               const weaponType = gear.twoHanded ? 'twoHanded' : 'oneHanded'
+
+               if (isObjEmpty(topGear[stat].weapon[weaponType])) {
+                  topGear[stat].weapon[weaponType] = gear
+               } else if (gear[stat] > topGear[stat].weapon[weaponType][stat]) {
+                  topGear[stat].weapon[weaponType] = gear
+               }
+            } else {
+               // gear.type !== 'weapon'
+               if (isObjEmpty(topGear[stat][gearType])) {
+                  topGear[stat][gearType] = gear
+               } else if (gear[stat] > topGear[stat][gearType][stat]) {
+                  topGear[stat][gearType] = gear
+               }
             }
          }
       })
    })
+   stats.forEach((stat) => {
+      const arr = []
+      habatica.gearTypes.forEach((gearType) => {
+         const gear = topGear[stat][gearType]
+         // let payload = []
+         if (!isObjEmpty(gear)) {
+            if (gearType === 'weapon') {
+               if (!isObjEmpty(gear.oneHanded)) {
+                  arr.push(gear.oneHanded)
+               }
+               if (!isObjEmpty(gear.twoHanded)) {
+                  arr.push(gear.twoHanded)
+               }
+            } else {
+               arr.push(gear)
+            }
+         }
+      })
+      topGear[stat] = arr
+   })
    return topGear
+}
+
+const fillQuestList = (stat, bestQuests, gears) => {
+   bestQuests.innerHTML = gears[stat]
+      .map((gear) => {
+         // Build gear type HTML
+         let gearTypeHTML = `<strong>Gear Type: </strong>${gear.type}`
+         if (gear.type === 'weapon') {
+            gearTypeHTML += gear.twoHanded ? ` (Two Handed)` : ` (One Handed)`
+         }
+
+         // Build stats HTML
+         const stats = ['int', 'str', 'per', 'con'] // Define the stats array
+         const gearStatsHTML = () => {
+            let statStr = '<strong>Stats:</strong><br>'
+            stats.forEach((statType) => {
+               if (gear[statType] > 0) {
+                  statStr += `&nbsp;&nbsp;&nbsp;&nbsp;<strong>${statType.toUpperCase()}:</strong> ${
+                     gear[statType]
+                  }<br>`
+               }
+            })
+            return statStr
+         }
+
+         // Build quest HTML
+         const questHTML = `
+         <strong>Quest:</strong> ${gear.quest.text}<br>
+         <strong>Quest Type:</strong> ${gear.quest.type}<br><br>
+       `
+
+         // Combine all parts and return
+         return gearTypeHTML + '<br>' + gearStatsHTML() + questHTML
+      })
+      .join('')
 }
 
 fetch('https://habitica.com/api/v3/content')
    .then((res) => res.json())
    .then((habaticaData) => {
       const gears = parseQuest(habaticaData.data)
-      console.log({ gears })
+
+      let stat = 'int'
+      const bestQuests = document.querySelector('#best-quests')
+
+      const btns = document.querySelectorAll(
+         '.stats-button-filter-wrapper button'
+      )
+
+      btns.forEach((btn) => {
+         btn.addEventListener('click', (e) => {
+            const newStat = e.target.getAttribute('data-stat')
+
+            // Remove active class
+            btns.forEach((btn) => btn.classList.remove('selected'))
+
+            // Add active class
+            e.target.classList.add('selected')
+
+            fillQuestList(newStat, bestQuests, gears)
+         })
+      })
+
+      fillQuestList(stat, bestQuests, gears)
    })
